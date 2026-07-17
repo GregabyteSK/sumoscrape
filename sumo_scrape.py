938163c08b985b709a48a81aa8e19b0642801e3d
GregabyteSK <greg@clockworkmayhem.com>
2026-07-17 18:13:19 -0500

    Fixed file detection logic

--- sumo_scrape.py
+++ sumo_scrape.py
@@ -20,7 +20,7 @@ def main():
             vod_json = requests.get(f"https://api.nhkworld.jp/showsapi/v1/en/video_episodes/{vod_id}")
             video_url = json.loads(vod_json.content)["video"]["url"]
             print(f"Found video: {day['title']}")
-            filename = os.path.join(url_date, day["title"])
+            filename = os.path.join(url_date, f'{day["title"]}.mp4')
             if not Path(filename).exists():
                 if save_video(video_url, filename):
                     print(f"File saved at {filename}")
@@ -38,9 +38,8 @@ def get_vod_id(url):
         if i.has_attr('data-vod'):
             return i.attrs['data-vod'].replace('-','')
 
-def save_video(url, title):
+def save_video(url, filename):
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36"
-    filename = f"{title}.mp4"
     command = f'ffmpeg -i {url} -user_agent "{user_agent}" -c copy "{filename}"'
     subprocess.call(command, shell=True)
     path = Path(filename)
